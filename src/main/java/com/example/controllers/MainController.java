1a5f8cd12f4f9d6258ed12f6954ed8f19b6bf3a4
celudirosa <cluqued@live.com>
2024-02-05 22:04:20 +0100

    add some detalles

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.services.EstudianteService;
@@ -20,12 +21,24 @@ public class MainController {
 
     private final Logger LOG = Logger.getLogger("MainController");
 
+    // listado de todos los estudiantes
     @GetMapping("/all")
     public String dameEstudiantes(Model model) {
         
         model.addAttribute("estudiantes", estudianteService.dameTodosLosEstudiantes());
         
         return "views/listadoEstudiantes";
+    }
+
+    // detalles de cada estudiante por ID
+    @GetMapping("/detalles/{id}")
+    public String detallesEstudiante(@PathVariable(name = "id") int idEstudiante, Model model) {
+
+        model.addAttribute(
+            "estudiante", 
+            estudianteService.dameUnEstudiante(idEstudiante));
+
+        return "views/estudianteDetalles";
     }
 
 }
