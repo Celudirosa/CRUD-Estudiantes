783a0bac95db2a828068bdf363313624ca6e7417
celudirosa <cluqued@live.com>
2024-02-05 22:40:38 +0100

    add correos y telefonos al formulario

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,5 +1,8 @@
 package com.example.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.entities.Correo;
 import com.example.entities.Estudiante;
+import com.example.entities.Telefono;
 import com.example.services.CursoService;
 import com.example.services.EstudianteService;
 
@@ -70,7 +75,44 @@ public class MainController {
         @RequestParam(name = "numerosTel", required = false) String telefonosRecibidos,
         @RequestParam(name = "direccionesCorreo", required = false) String correosRecibidos) {
 
-        
+        // procesar los telefonos
+        if (telefonosRecibidos != null) {
+            String[] arrayTelefonos = telefonosRecibidos.split(";");
+            List<String> numerosTelefonos = Arrays.asList(arrayTelefonos);
+
+            List<Telefono> telefonos = new ArrayList<>();
+            
+            numerosTelefonos.stream()
+                .forEach(numeroTelefono -> {
+                    telefonos.add(Telefono.builder()
+                    .telefono(numeroTelefono)
+                    .estudiante(estudiante)
+                    .build());
+                });
+
+            estudiante.setTelefonos(telefonos);
+        }
+
+        // procesar los correos
+        if (correosRecibidos != null) {
+            String[] arrayCorreos = correosRecibidos.split(";");
+            List<String> direccionesCorreos = Arrays.asList(arrayCorreos);
+
+            List<Correo> correos = new ArrayList<>();
+            
+            direccionesCorreos.stream()
+                .forEach(direccionCorreo -> {
+                    correos.add(Correo.builder()
+                    .correo(direccionCorreo)
+                    .estudiante(estudiante)
+                    .build());
+                });
+            
+            estudiante.setCorreos(correos);
+        }
+
+        estudianteService.persistirEstudiante(estudiante);
+
         return "redirect:/all";
     }
 
