731e19c224d7a0b9d75cd2d8ab85220d1560e724
celudirosa <cluqued@live.com>
2024-02-05 16:14:15 +0100

    add horario a curso

--- src/main/java/com/example/entities/Curso.java
+++ src/main/java/com/example/entities/Curso.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,9 +29,12 @@ public class Curso implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String nombre;
+    private String descripcion;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "departamento")
+    @Enumerated(EnumType.STRING) // con esto ya te saca el nombre
+    private Horario horario;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "curso")
     private List<Estudiante> estudiantes;
     
 }
