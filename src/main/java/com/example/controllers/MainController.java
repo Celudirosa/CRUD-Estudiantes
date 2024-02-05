8408686695bd9194f7bf054dbe5f98be118bf256
celudirosa <cluqued@live.com>
2024-02-05 22:20:45 +0100

    add persistirEstudiante

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -3,10 +3,14 @@ package com.example.controllers;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entities.Estudiante;
 import com.example.services.CursoService;
@@ -57,6 +61,16 @@ public class MainController {
             cursoService.dameCursos());
         
         return "views/frmAltaModificacion";
+    }
+
+    @PostMapping("/persistir")
+    @Transactional
+    public String persistirEstudiante (
+        @ModelAttribute(name = "estudiante") Estudiante estudiante,
+        @RequestParam(name = "numerosTel", required = false) String telefonosRecibidos,
+        @RequestParam(name = "direccionesCorreo", required = false) String correosRecibidos) {
+
+        return "redirect:/all";
     }
 
 }
